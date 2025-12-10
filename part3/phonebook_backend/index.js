8bbcd90c1e2cbdf43499c8730f122a4c5369d131
ThomasGirardet <127821779+ThomasGirardet@users.noreply.github.com>
2025-12-10 16:51:23 -0600

    Started Part 3
    This includes part 3.1 lecture content, exercises, and refactors the repository

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const numpersons = persons.length;
+  const date = new Date();
+
+  response.send(`
+    <p>Phonebook has info for ${numpersons} people</p>
+    <p>${date}</p>
+    `);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((note) => note.id === id);
+
+  if (person) response.json(person);
+  else response.status(404).end();
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId =
+    persons.length > 0
+      ? Math.max(...persons.map((person) => Number(person.id)))
+      : 0;
+
+  return String(maxId + 1);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number)
+    return response.status(400).json({
+      error: "Name or number missing",
+    });
+
+  if (persons.map((p) => p.name === body.name))
+    return response.status(400).json({
+      error: "Name must be unique",
+    });
+
+  const newPerson = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(newPerson);
+
+  response.json(newPerson);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
